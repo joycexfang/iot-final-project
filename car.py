520c27daa09707061b7120e709e1482a6b85911c
Joyce Fang <joycefang01@gmail.com>
2023-04-16 15:40:06 -0400

    add calculate_speed() function

--- car.py
+++ car.py
@@ -1,14 +1,20 @@
-class Car:
-  def __init__(self, rank, max_speed):
-    self.rank = rank
-    self.max_speed = max_speed
-    self.curr_speed = 0
+# Import math Library
+import math
 
-  def __str__(self):
-    return "CAR self.rank: {} \t self.max_speed: {} \t self.curr_speed: {}".format(self.rank, self.max_speed, self.curr_speed)
-  
-  def change_speed(self):
-    # todo: update code
-    self.curr_speed = 5
-    
-    print("changed self.curr_speed to:", self.curr_speed)
+class Car:
+    def __init__(self, rank, max_speed):
+        self.rank = rank
+        self.max_speed = max_speed
+        self.curr_speed = 0
+
+    def __str__(self):
+        return "CAR self.rank: {} \t self.max_speed: {} \t self.curr_speed: {}".format(self.rank, self.max_speed, self.curr_speed)
+
+    def change_speed(self):
+        # todo: update code
+        self.curr_speed = 5
+
+        print("changed self.curr_speed to:", self.curr_speed)
+
+    def calculate_speed(self, light_color):
+        return self.curr_speed - (light_color) * (1 - (1/math.pow(math.e, 1.5*self.rank))) * 1.2
