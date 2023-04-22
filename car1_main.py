e3d6b2a0e5e755c870ef58a243814f6b570bff34
Joyce Fang <55996042+joycexfang@users.noreply.github.com>
2023-04-22 14:33:00 -0400

    Make car object global so that all threads can modify it more easily (#1)
    * Update car1_main.py
    
    * remove car_object as a param
    
    * remove value from old proxy
    
    ---------
    
    Co-authored-by: andreshibata <andremassanobush@gmail.com>

--- car1_main.py
+++ car1_main.py
@@ -24,14 +24,16 @@ step_sequence = [[1,0,0,1],
                  [0,0,1,1],
                  [0,0,0,1]]
 
+car_object = Car(1,30)
 # main process for car 1 receiver
-def receiver(car_object):
+def receiver():
     # program for the car 1 receiver using Zigbee device
     while True:
         # Receive data
         print("CAR1_RECEIVER: Receiving data...")
 
         try:
+            global car_object
             xbee_message = car_object.receive_message()
             if xbee_message:
                 data = xbee_message.data
@@ -49,11 +51,11 @@ def receiver(car_object):
             
 
 # main process for car 1 transmitter
-def transmitter(car_object):
+def transmitter():
     while True:
         # Send data to another device
         print("CAR1_TRANSMITTER: Transmitting data...")
-
+        global car_object
         # check when car 1 needs to transmit data
         if car_object.get_need_transmit():
             message = car_object.get_msg_data
@@ -67,7 +69,7 @@ def transmitter(car_object):
             print()
 
 # main process for car 1 motor controller
-def motor_controller(car_object):
+def motor_controller():
     while True:
         # Control a motor or other actuator
         print("CAR1_MOTOR: Controlling motor...")
@@ -75,6 +77,7 @@ def motor_controller(car_object):
         # the meat
         i = 0
         for i in range(step_count):
+            global car_object
             for pin in range(0, len(car_object.motor_pins)):
                 GPIO.output( car_object.motor_pins[pin], step_sequence[car_object.motor_step_counter][pin] )
             if direction==True:
@@ -87,15 +90,16 @@ def motor_controller(car_object):
                 exit( 1 )
             time.sleep( car_object.step_sleep )
        
-        car_object.clean_up_motor_pins()
+        #car_object.clean_up_motor_pins()
         exit( 0 )
 
 def second_passed(oldepoch):
     return time.time() - oldepoch >= 1
 
-def refresh_speed(car_object):
+def refresh_speed():
     curr_oldtime = time.time()
     while True:
+        global car_object
         if second_passed(curr_oldtime):
             print("1 second passed")
             curr_oldtime = time.time()
@@ -112,13 +116,12 @@ if __name__ == "__main__":
     try:
         # Create a shared object for the car 1 object
         print("CAR1_MAIN: Creating Car 1...")
-        car1 = Car(1, 30)
 
         # Create threads for each function
-        receiver_thread = threading.Thread(target=receiver, args=(car1,))
-        transmitter_thread = threading.Thread(target=transmitter, args=(car1,))
-        motor_controller_thread = threading.Thread(target=motor_controller, args=(car1,))
-        refresh_speed_thread = threading.Thread(target=refresh_speed, args=(car1,))
+        receiver_thread = threading.Thread(target=receiver)
+        transmitter_thread = threading.Thread(target=transmitter)
+        motor_controller_thread = threading.Thread(target=motor_controller)
+        refresh_speed_thread = threading.Thread(target=refresh_speed)
 
         # Start all threads
         receiver_thread.setDaemon(True)
@@ -140,6 +143,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("CAR1_MAIN: Program stopped by user.")
         print("CAR1_MAIN: closing zigbee receiver AND transmitter, and cleaning up motor pins")
-        car1.value.close_zigbee_receiver()
-        car1.value.close_zigbee_transmitter()
-        car1.value.clean_up_motor_pins()
+        car_object.close_zigbee_receiver()
+        car_object.close_zigbee_transmitter()
+        car_object.clean_up_motor_pins()
