606f079b4ddbc234b5a3337294c1e4ff0f6527a3
Joyce Fang <joycefang01@gmail.com>
2023-04-22 10:35:52 -0400

    remove try and keyboard interrupt

--- car1_main.py
+++ car1_main.py
@@ -1,12 +1,19 @@
+"""
+car1_main.py is the main program used to receive and transmit messages from other
+devices on the network. it will run three process concurrently (receiver, transmitter, and motor controller)
+
+the flow of communication goes like so:
+traffic light transmits message -> car 1 receives message
+car 1 transmits message -> car 2 receives message
+"""
+
 import multiprocessing
 import time
 from class_car import Car
 import RPi.GPIO as GPIO
 
 step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
-
 direction = False # True for clockwise, False for counter-clockwise
-
 # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
 step_sequence = [[1,0,0,1],
                  [1,0,0,0],
@@ -38,7 +45,7 @@ def transmitter(car_proxy):
     while True:
         # Send data to another device
         print("Transmitting data...")
-      
+
 # main process for car 1 motor controller
 def motor_controller(car_proxy):
     car_object: Car = car_proxy.value
@@ -47,24 +54,20 @@ def motor_controller(car_proxy):
         print("Controlling motor...")
 
         # the meat
-        try:
-            i = 0
-            for i in range(step_count):
-                for pin in range(0, len(car_object.motor_pins)):
-                    GPIO.output( car_object.motor_pins[pin], step_sequence[car_object.motor_step_counter][pin] )
-                if direction==True:
-                    car_object.motor_step_counter = (car_object.motor_step_counter - 1) % 8
-                elif direction==False:
-                    car_object.motor_step_counter = (car_object.motor_step_counter + 1) % 8
-                else: # defensive programming
-                    print( "uh oh... direction should *always* be either True or False" )
-                    car_object.cleanup()
-                    exit( 1 )
-                time.sleep( car_object.step_sleep )
-        except KeyboardInterrupt:
-            car_object.cleanup()
-            exit( 1 )
-
+        i = 0
+        for i in range(step_count):
+            for pin in range(0, len(car_object.motor_pins)):
+                GPIO.output( car_object.motor_pins[pin], step_sequence[car_object.motor_step_counter][pin] )
+            if direction==True:
+                car_object.motor_step_counter = (car_object.motor_step_counter - 1) % 8
+            elif direction==False:
+                car_object.motor_step_counter = (car_object.motor_step_counter + 1) % 8
+            else: # defensive programming
+                print( "uh oh... direction should *always* be either True or False" )
+                car_object.cleanup()
+                exit( 1 )
+            time.sleep( car_object.step_sleep )
+       
         car_object.cleanup()
         exit( 0 )
 
@@ -89,3 +92,7 @@ if __name__ == "__main__":
     receiver_process.join()
     transmitter_process.join()
     motor_controller_process.join()
+
+    print("Closing car 1 zigbee receiver AND transmitter")
+    car1_proxy.value.close_zigbee_receiver()
+    car1_proxy.value.close_zigbee_transmitter()
